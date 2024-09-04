084e58b7d12c5b5db25615b99aabea3292475782
Nathan <nathan@cyberfreakz.com>
2024-09-04 16:34:20 +0100

    Fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,8 @@
-export default {
+module.exports = {
   extends: 'airbnb-base',
   rules: {
     'comma-dangle': ['error', 'never'],
     'object-curly-newline': ['error', { multiline: true, consistent: true }],
     semi: ['error', 'never']
-  },
-  env: {
-    jest: true
   }
 }
